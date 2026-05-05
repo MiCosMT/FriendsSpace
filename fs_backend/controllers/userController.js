2c4b6d1ea77a765e5199db36dacba6dd8c1640b1
MiCosMT <xrmartri675@ieshnosmachado.org>
2026-05-05 09:34:45 +0200

    Cambio

--- fs_backend/controllers/userController.js
+++ fs_backend/controllers/userController.js
@@ -21,8 +21,8 @@ const generarToken = (usuario) =>
 
 const cookieOpts = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  secure: false,
+  sameSite: "lax",
   maxAge: 1000 * 60 * 60,
 };
 
